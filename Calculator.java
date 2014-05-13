32780e119c672e25555b2306caf0c542536ed6ac
shayan2013 <shayan2013@hotmail.de>
2014-05-13 11:23:12 +0200

    benutzer eingabe

--- Calculator.java
+++ Calculator.java
@@ -5,7 +5,10 @@ public class Calculator{
 public static void main(String[] args){
 Scanner scanner = new Scanner(System.in);
 System.out.println("bitte eingeben [+-/*] zahl1 zahl2");
-
+String operator = scanner.next();
+int zahl1 = scanner.nextInt();
+int zahl2 = scanner.nextInt();
+float result = 0.0f;
 
 
 }
